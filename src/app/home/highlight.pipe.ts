7906d93f90cbb7edca64a7849bc395c5a1127aeb
Andres Herrera <andres.herrera@davincitech.co>
2019-07-26 11:46:24 -0500

    negrilla en los textos

--- src/app/home/highlight.pipe.ts
+++ src/app/home/highlight.pipe.ts
@@ -12,11 +12,38 @@ constructor(private _sanitizer: DomSanitizer) { }
         if (!list) { return []; }
         if (!searchText) { return list; }
         const arrayPalabras = searchText.split(' ');
+        debugger;
         for (let i = 0; i < arrayPalabras.length; i++) {
-            list = list.replace(
-                arrayPalabras[i], `<span style='font-weight:bold'>${arrayPalabras[i]}</span>` );
+            let format = this.formatCaracter(list);
+            format.toLowerCase();
+
+            format = format.replace(
+                arrayPalabras[i].toLowerCase(), `<span style='font-weight:bold'>${arrayPalabras[i].toLowerCase()}</span>` );
+
+            for (let j = 0; j < list.split('').length; j++) {
+                
+            }
         }
 
         return this._sanitizer.bypassSecurityTrustHtml(list);
+    }
+    formatCaracter(str): string {
+        const from = 'ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç',
+            to   = 'AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc',
+            mapping = {};
+
+        for (let i = 0, j = from.length; i < j; i++ ) {
+            mapping[ from.charAt( i ) ] = to.charAt( i );
+        }
+        const ret = [];
+        for ( let i = 0, j = str.length; i < j; i++ ) {
+            const c = str.charAt( i );
+            if ( mapping.hasOwnProperty( str.charAt( i ) ) ) {
+                ret.push( mapping[ c ] );
+            } else {
+                ret.push( c );
+            }
+        }
+        return ret.join( '' );
     }
 }
