c5244e4520bae9a3741e8a19573ed4d0c5f55b13
Daniel Bastidas <daniel.bastidas@davincitech.co>
2019-11-04 05:11:57 -0500

    Historial de busquedas

--- src/app/historial-usuario/historial-usuario.component.ts
+++ src/app/historial-usuario/historial-usuario.component.ts
@@ -1,9 +1,13 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ResponseSearch } from '../models/response-search';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { HistorialUsuariosService } from '../providers/historial-usuarios.service';
 import { matTableFilter } from '../../common/matTableFilter';
+import { AjaxService } from '../providers/ajax.service';
+import { UserService } from '../providers/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { QuillService } from '../providers/quill.service';
 
 @Component({
   selector: 'app-historial-usuario',
@@ -12,115 +16,43 @@ import { matTableFilter } from '../../common/matTableFilter';
 })
 export class HistorialUsuarioComponent implements OnInit {
 
-  data: any; // informacion del back por el servicio de buscar historial x usuario
-  displayedColumns = ['fecha', 'busqueda', 'tipoBusqueda', 'url']; // culmnas tabla
-  dataSource; // data table
-
-  numeroPage = 1; // numero de paginas para la paginacion
-  totalPages = 1; // total de paginas obtenidas en el servicio
-  color = '#000';
-  colorR = '#eee';
-  matTableFilter:matTableFilter;
+  displayedColumns = ['texto_busqueda', 'tipo_busqueda', 'fecha_busqueda', 'url'];
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+  @ViewChild(MatSort)
+  sort: MatSort;
+  dataSource = new MatTableDataSource([]);
+  matTableFilter: matTableFilter;
   filterColumns = [
-    {field:'idtbl_motivo_cierre_chat', type:'number'},
-    {field: 'nombre', type:'string'}
+    { field: 'texto_busqueda', type: 'string' },
+    { field: 'tipo_busqueda', type: 'string' },
+    { field: 'fecha_busqueda', type: 'string' },
+    { field: 'url', type: 'string' }
   ];
+  user;
+  busquedas: any;
+  constructor(private ajax: AjaxService, private userService: UserService, private route: ActivatedRoute, private router: Router, private cg: ChangeDetectorRef, private qs: QuillService) {
 
-  @ViewChild(MatSort) sort: MatSort; // sort para ordenar
-  @ViewChild(MatPaginator) paginator: MatPaginator; // paginator
- 
-  /**
-   * Set the sort after the view init since this component will
-   * be able to query its view for the initialized sort.
-   */
-  constructor(
-    private responseSearch: ResponseSearch,
-    private historialUsuariosService: HistorialUsuariosService,
-    breakpointObserver: BreakpointObserver
-  ) {
-    this.responseSearch.setActiveMostrarBarra(false);
-    breakpointObserver.observe(['(max-width: 600px)']).subscribe(result => {
-      this.displayedColumns = result.matches ?
-        ['fecha', 'busqueda', 'tipoBusqueda', 'url'] :
-        ['fecha', 'busqueda', 'tipoBusqueda', 'url'];
+    this.user = this.userService.getUsuario();
+    this.userService.observableUsuario.subscribe(u => {
+      if (u) {
+        this.user = u;
+      }
     });
-
-   /*  this.data = this.responseSearch._historial.map(data => {
-      data.busqueda = data.busqueda.toLowerCase();
-      data.tipoBusqueda = data.tipoBusqueda.toLowerCase();
-      data.url = data.url.toLowerCase();
-      return data;
-    }); */
-    
   }
 
-  applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
-  }
 
   ngOnInit() {
-    this.buscarBusquedaHistorial('');
-  }
-  /**
-    * @param  {} tipo tipo de boton que ejecuta la funcion
-    * buscar el historial de busqueda que se devuelve con la paginacion
-  */
-  buscarBusquedaHistorial(tipo) {
-    if (tipo === 'left') {
-      this.numeroPage--;
-    } else if (tipo === 'right') {
-      this.numeroPage++;
-    }
-    this.historialUsuariosService.getHistorialUsuarios({numPagina : this.numeroPage, user_id : 1}).subscribe((data) => {
-      this.data = data;
-      if (this.data.length > 0) {
-        this.totalPages = Math.ceil(this.data[0].numPaginas);
-      }
-      this.dataSource = new MatTableDataSource(this.data);
-      this.dataSource.sort = this.sort;
+    
+    this.userService.obtenerHistorialBusquedas(this.user.idtbl_usuario).then(p => {
+      this.busquedas = p;
+      this.dataSource = new MatTableDataSource(this.busquedas);
       this.dataSource.paginator = this.paginator;
-      this.matTableFilter = new matTableFilter(this.dataSource,this.filterColumns);
+      this.dataSource.sort = this.sort;
+      this.matTableFilter = new matTableFilter(this.dataSource, this.filterColumns);
     });
+
   }
-  /**
-     * Asignar valores al sort y paginacion
-  */
-  ngAfterViewInit() {
-    // this.dataSource.sort = this.sort;
-    // this.dataSource.paginator = this.paginator;
-  }
-  /**
-     *Validar la paginacion boton izquierda
-  */
-  validarLeft() {
-    if (this.numeroPage <= 1) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  /**
-     *Validar la paginacion boton derecha
-  */
-  validarRight() {
-    if(this.numeroPage >= this.totalPages) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  /**
-    * @param  {} event petecion
-    *Ordenar por fecha
-  */
-  sortData(event) {
-    this.data = this.data.sort((a, b) => {
-      const fecha1 = new Date(a.fecha_busqueda).valueOf();
-      const fecha2 = new Date(a.fecha_busqueda).valueOf();
-      return fecha1 > fecha2 ? 1 : -1;
-    });
-  }
+
 
 }
