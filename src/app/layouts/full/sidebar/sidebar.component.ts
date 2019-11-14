6245f568b3171d49f16288c270ff22d6d3aa6fd3
NatalieSantiago <natalie.santiago@davincitech.co>
2019-11-14 17:26:25 -0500

    ajustes

--- src/app/layouts/full/sidebar/sidebar.component.ts
+++ src/app/layouts/full/sidebar/sidebar.component.ts
@@ -65,10 +65,12 @@ export class AppSidebarComponent implements OnDestroy {
 
   deslogueo() {
     this.autenticationService.logOut();
-    this.state.id_usuario_experto = this.user.getId();
-    this.state.id_estado_experto_actual = this.user.getEstadoExpertoActual();
-    this.state.estado_ingreso = 0;
-    this.estadoExpertoService.createLogState(this.state);
+    if(this.user && this.user.getIdRol() == 2) {
+      this.state.id_usuario_experto = this.user.getId();
+      this.state.id_estado_experto_actual = this.user.getEstadoExpertoActual();
+      this.state.estado_ingreso = 0;
+      this.estadoExpertoService.createLogState(this.state);
+    }
   }
 
   cerrarMenu() {
