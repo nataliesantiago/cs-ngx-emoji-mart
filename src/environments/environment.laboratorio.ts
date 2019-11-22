11ac69b85b6530fae0ce02b7e60fdc4c0254d456
david.tobar@davincitech.co <david.tobar@davincitech.co>
2019-11-22 08:55:00 -0500

    up

---
+++ src/environments/environment.laboratorio.ts
@@ -0,0 +1,36 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  URL_BACK: 'https://comusoporte-desarrollo.appspot.com/api/',
+  CLIENT_ID: '993073337079-0qt03a4l3pk7psfnpk1nto8m5bt14vnt.apps.googleusercontent.com', //id de cliente de google para la aplicacion
+  firebaseConfig: {
+    apiKey: "AIzaSyBEKRnunoggWof_mmJDfHRG9-fgKYfYOF8",
+    authDomain: "comusoporte-desarrollo.firebaseapp.com",
+    databaseURL: "https://comusoporte-desarrollo.firebaseio.com",
+    projectId: "comusoporte-desarrollo",
+    storageBucket: "",
+    messagingSenderId: "537955814682",
+    appId: "1:537955814682:web:b726049a21de1c69"
+  },
+  ambiente: 'col',
+  pais: {
+    col: {
+      id_origen_conecta: 'datasources/2721fbfe980dfcd21c1c5aece9cacef5',
+      id_origen_drive: 'datasources/2721fbfe980dfcd20dfd92cb9961df38',
+      id_origen_chat: 'datasources/2721fbfe980dfcd221c70095e8d11769'
+    }
+  },
+  analytics: `<script async src="https://www.googletagmanager.com/gtag/js?id=CK3PS915EF"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'CK3PS915EF');
+  </script>`
+};
+
