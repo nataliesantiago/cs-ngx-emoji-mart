a279369ecb5357af9499edb1ff85463939659535
Daniel Bastidas <daniel.bastidas@davincitech.co>
2019-12-09 12:40:16 -0500

    Subir cambios

--- src/app/respuestas/respuestas.component.ts
+++ src/app/respuestas/respuestas.component.ts
@@ -93,6 +93,26 @@ export class RespuestasComponent implements OnInit {
           this.pregunta_nueva = true;
         }
 
+        this.ajax.get('preguntas/obtener-preguntas-asociadas', { idtbl_pregunta: this.id_pregunta_visualizar }).subscribe(pras => {
+          if (pras.success) {
+            this.preguntas_adicion = pras.preguntas_asociadas;
+            console.log(pras);
+            if (this.preguntas_adicion.length < 5) {
+              this.searchService.queryCloudSearch(this.pregunta.titulo, 1, 'conecta', 0, false).then(asociadas => {
+                console.log(asociadas);
+                if (asociadas.results) {
+                  asociadas.results.filter(f => {
+                    return f.idtbl_pregunta != this.pregunta.idtbl_pregunta;
+                  }).forEach(a => {
+                    this.preguntas_adicion.push({ titulo: a.title, idtbl_pregunta: a.idtbl_pregunta });
+                  });
+                }
+              })
+            }
+            //this.cg.detectChanges();
+          }
+        });
+
         this.ajax.get('preguntas/obtener-subrespuesta', { idtbl_pregunta: this.id_pregunta_visualizar }).subscribe(sr => {
           if (sr.success) {
 
@@ -155,26 +175,6 @@ export class RespuestasComponent implements OnInit {
                         }
                       }
                     }
-
-                    this.ajax.get('preguntas/obtener-preguntas-asociadas', { idtbl_pregunta: this.id_pregunta_visualizar }).subscribe(pras => {
-                      if (pras.success) {
-                        this.preguntas_adicion = pras.preguntas_asociadas;
-                        console.log(pras);
-                        if (this.preguntas_adicion.length < 5) {
-                          this.searchService.queryCloudSearch(this.pregunta.titulo, 1, 'conecta', 0, false).then(asociadas => {
-                            console.log(asociadas);
-                            if (asociadas.results) {
-                              asociadas.results.filter(f => {
-                                return f.idtbl_pregunta != this.pregunta.idtbl_pregunta;
-                              }).forEach(a => {
-                                this.preguntas_adicion.push({ titulo: a.title, idtbl_pregunta: a.idtbl_pregunta });
-                              });
-                            }
-                          })
-                        }
-                        //this.cg.detectChanges();
-                      }
-                    });
                   }
                 })
               }
