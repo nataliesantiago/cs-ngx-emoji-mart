2238bb1c168eb520c6d62fd90332f40ee7349369
NatalieSantiago <natalie.santiago@davincitech.co>
2020-06-04 14:45:53 -0500

    remove files

--- src/app/models/webkitrecognize.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface IWindow extends Window {
-    webkitSpeechRecognition: any;
-}
--- src/app/pipes/mensajes-experto.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MensajesExpertoPipe } from '../../pipes/mensajes-experto.pipe';
-
-describe('MensajesExpertoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MensajesExpertoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
